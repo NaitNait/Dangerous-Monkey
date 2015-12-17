28d71c977d403e503b67837901e957774e944f77
rehanhajee <rehanhajee@gmail.com>
2015-12-17 13:32:42 -0500

    Create HighScores.java

---
+++ HighScores.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class HighScores{
+	/*
+	Scanner scan
+	if (Scanner.hasNext())
+		Scanner.next();	
+	if (Scanner.hasNextInt())
+		Scanner.nextInt();
+	Scanner.nextLine();
+	*/
+}
