653b1641ad305f95efd8045b7afb7959601d6729
rehanhajee <rehanhajee@gmail.com>
2016-01-10 20:14:29 -0500

    Update and rename Main.java to MainWindow.java

--- Main.java
+++
@@ -1,5 +0,0 @@
-class Main { /*implements ActionListener, KeyListener */
-	public static void main(String[] args){
-		new MainMenu();
-	}
-}
---
+++ MainWindow.java
@@ -0,0 +1,18 @@
+/**
+ * MainWindow class
+ *
+ * All operations of the GUI will go through this class
+ * 
+ * @author Rehan Hajee
+ **/
+ import java.io.*;
+
+class MainWindow /*implements ActionListener, KeyListener, MouseListener */ {
+	public MainWindow(){
+		
+	}
+	
+	public static void main(String [] args) throws IOException{
+		new MainMenu();
+	}
+}//end MainWindow
