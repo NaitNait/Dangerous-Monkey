c241ca760db2b57479f34480e4146e8cc9527341
rehanhajee <rehanhajee@gmail.com>
2016-01-17 00:18:51 -0500

    Update Ship.java

--- Ship.java
+++ Ship.java
@@ -10,13 +10,21 @@
 import java.awt.*;
 import java.awt.event.*;
 import java.awt.geom.*;
+import java.awt.image.*;
+import java.io.*;
 import javax.swing.*;
+import javax.imageio.*;
 
 public class Ship extends JFrame implements KeyListener {
 	
 	//setting coordinates of the ship from top left going counter-clockwise
+	//static int shipX[] = {20, 20, 28, 40, 48, 48, 40, 40, 28, 28};
 	static int shipX[] = {20, 20, 28, 40, 48, 48, 40, 40, 28, 28};
 	static int shipY[] = {60, 80, 88, 88, 80, 60, 60, 80, 80, 60};
+	//static int shipY[] = {60, 80, 88, 88, 80, 60, 60, 80, 80, 60};
+	
+	static int backgroundX = -159;
+	static int backgroundY = -1105;
 	
 	static double averageX = 0.0;
 	static double averageY = 0.0;
@@ -26,11 +34,20 @@ public class Ship extends JFrame implements KeyListener {
 	
 	DrawPanel pnlGraphics = new DrawPanel();
 	
+	//ImageIcon img = new ImageIcon("img\\Background.jpg");
+	static BufferedImage backgroundImg = null;
+	static int imgHeightAvg = 0;
+	static int imgWidthAvg = 0;
+	
 	//setting ship constructor
 	public Ship() {
 		super("Asteroids");
-		
-		pnlGraphics.setPreferredSize(new Dimension(800, 500));
+		try {
+			backgroundImg = ImageIO.read(new File("img\\Background.jpg"));
+		} catch (IOException e){
+			System.out.println("The following problem writing to a file occurred:\n" + e);
+		}
+		pnlGraphics.setPreferredSize(new Dimension(800, 600));
 		
 		addKeyListener(this);
 		
@@ -52,6 +69,9 @@ public class Ship extends JFrame implements KeyListener {
 		public void paintComponent (Graphics g) {
 			super.paintComponent(g);
 			
+			g.drawImage(backgroundImg, backgroundX, backgroundY, null);
+			//System.out.println(backgroundX + ", " + backgroundY);
+			
 			//resetting averageX and averageY to zero
 			averageX = averageY = 0.0;
 			for (int i = 0; i < shipX.length;i++){
@@ -71,7 +91,8 @@ public class Ship extends JFrame implements KeyListener {
 			g2.setTransform(afx);
 			g.setColor(Color.blue);
 			g2.fillPolygon(shipX, shipY, 10);
-			System.out.println(angle + " sin θ = " + Math.sin(Math.toRadians(angle)));
+			System.out.println(shipY[0] + ", " + shipY[5]);
+			System.out.println(angle + " sin (" + (angle /**+ 90*/) + ") = " + Math.sin(Math.toRadians(angle /**+ 90*/ )));
 			//restore old rotation seting
 			g2.setTransform(old);
 			// everything after this is not rotated
@@ -87,63 +108,129 @@ public class Ship extends JFrame implements KeyListener {
 	}//end keyReleased
 	public void keyTyped(KeyEvent e) {}
 	public void keyPressed(KeyEvent e) {
+		
+		
 		//if arrow up is pressed
 		if (e.getKeyCode() == KeyEvent.VK_UP) {
-			if (angle % 360 == 0){ //if the angle is divisible by 360
+			
+			if (shipX[0] >= 7 && shipX[0] <= 777 && shipY[0] >= 2 && shipY[0] <= 582){
+			
 				for (int i = 0; i < shipX.length; i++) {
-				shipY[i] -= 10;
+					shipX[i] -= Math.cos(Math.toRadians(angle + 90))*10;
+					shipY[i] -= Math.sin(Math.toRadians(angle + 90))*10;
 				}//end for loop
-			} else if (Math.cos(Math.toRadians(angle+90)) >= 0) { //else if cos(angle+90) is greater than or equal to zero
-				angle += 7.5;
-			} else { //else if cos(angle+90) is less than zero
-				angle -= 7.5;
-			}//end if/else
-			pnlGraphics.repaint();
-		}//end if
-		//if arrow down is pressed
-		if (e.getKeyCode() == KeyEvent.VK_DOWN) {
 			
-			if ((angle-180) % 360 == 0){ //if the angle minus 180 is divisible by 360
-				for (int i = 0; i < shipX.length; i++) {
-				shipY[i] += 10;
+				backgroundX += Math.cos(Math.toRadians(angle + 90))*10;
+				backgroundY += Math.sin(Math.toRadians(angle + 90))*10;
+
+				if (Math.round(backgroundX / 10) * 10 == -30)
+					backgroundX = -356;
+				else if (Math.round(backgroundX / 10) * 10 == -550)
+					backgroundX = -356;
+				if (Math.round(backgroundY / 10) * 10 -5 == -395)
+					backgroundY = -1105;
+				else if (Math.round(backgroundY / 10) * 10 == -1750)
+					backgroundY = -340;
+			}
+			else {
+				if (shipX[0] < 7){
+					if (Math.sin(Math.toRadians(angle)) >= 0){
+						for (int i = 0; i < shipX.length; i++) {
+							shipX[i] -= Math.cos(Math.toRadians(angle + 90))*10;
+							shipY[i] -= Math.sin(Math.toRadians(angle + 90))*10;
+						}//end for loop
+			
+						backgroundX += Math.cos(Math.toRadians(angle + 90))*10;
+						backgroundY += Math.sin(Math.toRadians(angle + 90))*10;
+
+						if (Math.round(backgroundX / 10) * 10 == -30)
+							backgroundX = -356;
+						else if (Math.round(backgroundX / 10) * 10 == -550)
+							backgroundX = -356;
+						if (Math.round(backgroundY / 10) * 10 -5 == -395)
+							backgroundY = -1105;
+						else if (Math.round(backgroundY / 10) * 10 == -1750)
+							backgroundY = -340;
+					}
 				}
-			} else if (Math.cos(Math.toRadians(angle+90)) >= 0) { //else if cos(angle+90) is greater than or equal to zero
-				angle -= 7.5;
-			} else { //else if cos(angle+90) is less than zero
-				angle += 7.5;
-			}//end if/else
+				if (shipX[0] > 777){
+					if (Math.sin(Math.toRadians(angle)) <= 0){
+						for (int i = 0; i < shipX.length; i++) {
+							shipX[i] -= Math.cos(Math.toRadians(angle + 90))*10;
+							shipY[i] -= Math.sin(Math.toRadians(angle + 90))*10;
+						}//end for loop
 			
+						backgroundX += Math.cos(Math.toRadians(angle + 90))*10;
+						backgroundY += Math.sin(Math.toRadians(angle + 90))*10;
+
+						if (Math.round(backgroundX / 10) * 10 == -30)
+							backgroundX = -356;
+						else if (Math.round(backgroundX / 10) * 10 == -550)
+							backgroundX = -356;
+						if (Math.round(backgroundY / 10) * 10 -5 == -395)
+							backgroundY = -1105;
+						else if (Math.round(backgroundY / 10) * 10 == -1750)
+							backgroundY = -340;
+					}
+				}
+				if (shipY[0] < 2){
+					if (Math.cos(Math.toRadians(angle)) <= 0){
+						for (int i = 0; i < shipX.length; i++) {
+							shipX[i] -= Math.cos(Math.toRadians(angle + 90))*10;
+							shipY[i] -= Math.sin(Math.toRadians(angle + 90))*10;
+						}//end for loop
+			
+						backgroundX += Math.cos(Math.toRadians(angle + 90))*10;
+						backgroundY += Math.sin(Math.toRadians(angle + 90))*10;
+
+						if (Math.round(backgroundX / 10) * 10 == -30)
+							backgroundX = -356;
+						else if (Math.round(backgroundX / 10) * 10 == -550)
+							backgroundX = -356;
+						if (Math.round(backgroundY / 10) * 10 -5 == -395)
+							backgroundY = -1105;
+						else if (Math.round(backgroundY / 10) * 10 == -1750)
+							backgroundY = -340;
+					}
+				}
+				if (shipY[0] > 582){
+					if (Math.cos(Math.toRadians(angle)) >= 0){
+						for (int i = 0; i < shipX.length; i++) {
+							shipX[i] -= Math.cos(Math.toRadians(angle + 90))*10;
+							shipY[i] -= Math.sin(Math.toRadians(angle + 90))*10;
+						}//end for loop
+			
+						backgroundX += Math.cos(Math.toRadians(angle + 90))*10;
+						backgroundY += Math.sin(Math.toRadians(angle + 90))*10;
+
+						if (Math.round(backgroundX / 10) * 10 == -30)
+							backgroundX = -356;
+						else if (Math.round(backgroundX / 10) * 10 == -550)
+							backgroundX = -356;
+						if (Math.round(backgroundY / 10) * 10 -5 == -395)
+							backgroundY = -1105;
+						else if (Math.round(backgroundY / 10) * 10 == -1750)
+							backgroundY = -340;
+					}
+				}
+			}
 			pnlGraphics.repaint();
 		}//end if
+		
 		//if right arrow is pressed
-		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-			
-			if ((angle - 90) % 360 == 0) { //if the angle minus 90 is divisible by 360
-				for (int i = 0; i < shipX.length; i++) {
-					shipX[i] += 10;
-				}
-			}  else if (Math.sin(Math.toRadians(angle+90)) >= 0) { //else if sin(angle+90) is greater than or equal to zero
-				angle += 7.5;
-			} else if (Math.sin(Math.toRadians(angle+90)) < 0 ) { //else if sin(angle+90) is less than zero
-				angle -= 7.5;
-			}//end if/else
+		else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+		
+			angle += 7.5;
 			
 			pnlGraphics.repaint();
 			
 		}//end if
 		//if left arrow is pressed
 		if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-			if ((angle - 270) % 360 == 0) {
-				for (int i = 0; i < shipX.length; i++) {
-					shipX[i] -= 10;
-				}
-			} else if (Math.sin(Math.toRadians(angle+90)) >= 0 ){ //else if sin(angle+90) is greater than or equal to zero
-				angle -= 7.5;
-			} else if (Math.sin(Math.toRadians(angle+90)) < 0){ //else if sin(angle+90) is less than zero
-				angle += 7.5;
-			}//end if/else
+			
+			angle -= 7.5;
+			
 			pnlGraphics.repaint();
 		}//end if
-		
 	}//end keyPressed
 }//end Ship
