c65b83ae426b5df3ba8a6185175ba2798a54b2dd
rehanhajee <rehanhajee@gmail.com>
2016-01-18 23:15:49 -0500

    Update HighScore.java

--- HighScore.java
+++ HighScore.java
@@ -4,11 +4,12 @@
  * Gets of the high scores of the game
  * This class includes an ArrayList, and reading and writing to files
  * 
- * @author Rehan Hajee
+ * #rehan
  **/
 import java.io.*;
 import java.util.*;
 
+//#method
 class HighScore {
 	
 	//initialize variables
@@ -19,6 +20,13 @@ class HighScore {
 	
 	static ArrayList<HighScore> arrHighScoreList = new ArrayList<HighScore>();
 	
+	//#define
+	/**
+	 * HighScore method
+	 * Creates high score object
+	 * @param Sring a - name of the person
+	 * @param int b - score the person got
+	 **/
 	public HighScore (String a, int b) {
 		this.name = a;
 		this.score = b;
@@ -26,47 +34,62 @@ class HighScore {
 	
 	
 	public static void getScores() throws IOException {
+		//#error
 		try {
 			Scanner sc = new Scanner(file);
 			while (sc.hasNextInt()) {
 				int num = sc.nextInt();
 				String line = sc.nextLine();
+				//#define
 				HighScore hScore = new HighScore(line, num);
 				arrHighScoreList.add(hScore);
 			}//end while
 			sc.close();
-		} catch (Exception ex){
+		} catch (Exception ex) {
 			System.out.println("Error is:" + ex);
 		}//end try/catch
+
 		sortScores();
 		writeScores();
+		
 	}//end getScores
 	
-	public static void sortScores(){
-		for (int i = 1; i < arrHighScoreList.size(); i++){
+	// #alg
+	public static void sortScores() {
+		for (int i = 1; i < arrHighScoreList.size(); i++) {
 			HighScore temp = arrHighScoreList.get(i);
 			int j = i;
-			while (j > 0 && arrHighScoreList.get(j-1).score < temp.score){
+			while (j > 0 && arrHighScoreList.get(j-1).score < temp.score) {
 				arrHighScoreList.set(j, arrHighScoreList.get(j-1));
 				j--;
-			}
+			}//end while
 			arrHighScoreList.set(j, temp);
 		}//end for loop
+			while (arrHighScoreList.size() > 20) {
+				arrHighScoreList.remove(arrHighScoreList.size() - 1);
+			}//end while
 	}//end sortScores
 	
+	//#save
 	public static void writeScores() throws IOException {
 		FileWriter fw = new FileWriter(file);
 		PrintWriter pw = new PrintWriter(fw);
-		for (int i  = 0; i < arrHighScoreList.size(); i++){
-			pw.println(arrHighScoreList.get(i).score + arrHighScoreList.get(i).name);
-		}
-		pw.close();		
+		if (Main.cheatEnabled){
+			for (int i  = 0; i < 20; i++) {
+				pw.println(arrHighScoreList.get(i).score + arrHighScoreList.get(i).name);
+			}//end for
+			
+		}//end if
+		
+		pw.close();
+		
 	}//end writeScores
 
-	
-	public static void printScores(){
-		for (int i = 0; i < arrHighScoreList.size();i++){
+	public static void printScores() {
+		for (int i = 0; i < arrHighScoreList.size();i++) {
 			System.out.println(arrHighScoreList.get(i).score + "," + arrHighScoreList.get(i).name);
-		}//end for loop*/
+		}//end for loop
+		
 	}//end printScores
+	
 }//end HighScore
