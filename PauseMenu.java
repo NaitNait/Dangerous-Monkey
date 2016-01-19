c77c48d23ff37b1117ae8314338c985a4b7981fe
rehanhajee <rehanhajee@gmail.com>
2016-01-18 23:17:00 -0500

    Update PauseMenu.java

--- PauseMenu.java
+++ PauseMenu.java
@@ -4,7 +4,7 @@
  * Creates the pause menu of the game containing 1 JLabel and 5 JButtons
  * This class includes an action listener, key listener, and file writer
  * 
- * @author Rehan Hajee
+ * #rehan
  **/
 
 import java.awt.*;
@@ -12,7 +12,7 @@ import java.awt.event.*;
 import javax.swing.*;
 import java.io.*;
 
-
+//#method
 class PauseMenu extends JFrame implements ActionListener, KeyListener {
 
 	//create JLabel
@@ -41,7 +41,7 @@ class PauseMenu extends JFrame implements ActionListener, KeyListener {
 		super("Asteroids");
 		
 		//Configures each JButton and adds it to the button list
-		for(int i = 0; i < buttonList.length;i++){
+		for(int i = 0; i < buttonList.length;i++) {
 			buttonList[i] = new JButton(buttonText[i]);
 			buttonList[i].setPreferredSize(new Dimension(200, 40));
 			buttonList[i].setFocusPainted(false);
@@ -70,45 +70,46 @@ class PauseMenu extends JFrame implements ActionListener, KeyListener {
 		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	}//end PauseMenu constructor
 
+	//#action
 	public void actionPerformed(ActionEvent e) {
-		if (buttonList[0] == e.getSource()){
+		if (buttonList[0] == e.getSource()) {
 			pause = !pause;
 			dispose();
-		}
+		}//end if
+		
 		if (buttonList[1] == e.getSource()) {
 			dispose();
 			
-			//resetting coordinates of the ship from top left going counter-clockwise
+			//resetting all variables to their original
 			Main.shipX = new int[] {400, 400, 408, 420, 428, 428, 420, 420, 408, 408};
 			Main.shipY = new int[] {300, 320, 328, 328, 320, 300, 300, 320, 320, 300};
-			
 			Main.backgroundX = -159;
 			Main.backgroundY = -1105;
-			
 			Main.averageX = 0.0;
 			Main.averageY = 0.0;
-			
-			//resetting angle of rotation for the ship
 			Main.angle = 0.0;
 			
 			Main.pnlGraphics.repaint();
-		}
-		if (buttonList[2] == e.getSource()){
+		}//end if
+		
+		if (buttonList[2] == e.getSource()) {
 			JFileChooser chooseFile = new JFileChooser();
 			chooseFile.setDialogTitle("Specify a file to save");
 
+			//#save
 			int userSelection = chooseFile.showSaveDialog(this);
-			if (userSelection == JFileChooser.APPROVE_OPTION)
-			{
+			if (userSelection == JFileChooser.APPROVE_OPTION) {
+				
 				//Attempt to save to the file
+				//#error
 				File file = chooseFile.getSelectedFile();
 				try {
 					FileWriter fw = new FileWriter(file);
 					PrintWriter pw = new PrintWriter(fw);
-					for (int i  = 0; i < Main.shipX.length; i++){
+					for (int i  = 0; i < Main.shipX.length; i++) {
 						pw.print(Main.shipX[i] + " ");
 						pw.println(Main.shipY[i]);
-					}
+					}//end for
 					pw.println(Main.angle);
 					pw.println(Main.backgroundX + " " + Main.backgroundY);
 					pw.println(Main.cheatEnabled);
@@ -119,20 +120,24 @@ class PauseMenu extends JFrame implements ActionListener, KeyListener {
 				}//end try/catch
 			}//end if
 		}//end if
-		if (buttonList[3] == e.getSource()){
+		
+		if (buttonList[3] == e.getSource()) {
 			new Settings();
-		} 
-		if (buttonList[4] == e.getSource()){
+		}//end if
+		
+		if (buttonList[4] == e.getSource()) {
 			System.exit(0);
-		}
+		}//end if
+		
 	}//end actionPerformed
 	
-	public void keyReleased(KeyEvent e){
+	public void keyReleased(KeyEvent e) {
 		if (e.getKeyCode() == KeyEvent.VK_P || e.getKeyCode() == KeyEvent.VK_ESCAPE) {
 			pause = !pause;
 			dispose();
-		}
+		}//end if
 	}//end keyReleased
-	public void keyTyped(KeyEvent e){}
-	public void keyPressed(KeyEvent e){}
+	
+	public void keyTyped(KeyEvent e) {}
+	public void keyPressed(KeyEvent e) {}
 }
